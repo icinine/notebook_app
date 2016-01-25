cd2b2462b43902e8d71a0c21d0439151b6d6221f
icinine <shane.carr@sitesignals.com>
2016-01-25 20:20:52 +0000

    User Authenticated root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
-
-  root 'welcome#index'
   resources :notes
+  
+  #If a user is signed in, root them to the notes page.
+  #Cannot use 'root' alone as it gives Argument error. Auth root needs an alias
+  authenticated :user do
+    root 'notes#index', as: "authenticated_root"
+  end
+  #Else if not signed in send to normal root
+  root 'welcome#index'
+  
 end
